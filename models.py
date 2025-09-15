23e598c78801a0685d0e930db481f8fa302ea845
ledr11211 <ledr11211@gmail.com>
2025-09-15 21:31:05 +0300

    Initial commit

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     transactions = db.relationship('Transaction', backref='owner', lazy=True)
     email = db.Column(db.String(120), unique=True, nullable=True)
